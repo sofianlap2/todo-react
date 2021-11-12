018f05fc3ac5ff692e57c90e980f42e515878262
sofianenabli <sofienne.nabli@aleia.io>
2021-11-12 20:45:18 +0100

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import data from './utils/data.json';
+import './sass/todo.css';
 
 function App() {
+
+  const [tasksList, setTasksList] = useState(data);
+  const [newTask, setNewTask] = useState({
+    task : '',
+    id : '',
+    complete : '',
+  });
+
+  const onChangeHandler = (e) => {
+    setNewTask({
+      task : e.target.value,
+      id: tasksList.length + 1,
+      complete : true
+    })
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setTasksList([...tasksList, newTask]);
+    setNewTask({
+      task: '',
+    });
+  }
+
+  const delteHandler = (todo) => {
+    setTasksList(tasksList.filter(oneTask => oneTask.id !== todo.id))
+  }
+
+  const editHandler = (id, newName) => {
+    setTasksList(tasksList.map(singletask => {
+      if (singletask.id === id) {
+        singletask.task = newName;
+      }
+      return singletask;
+    }));
+  }
+
+  const completedHandler = (id) => {
+    setTasksList(tasksList.map(singletask => {
+      if (singletask.id === id) {
+        singletask.complete = !singletask.complete;
+      }
+      return singletask;
+    }));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo app</h1>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor="task">add new task</label>
+        <input value={newTask.task} id="task" name="task" placeholder="add task here" onChange={onChangeHandler}></input>
+        <button>ADD</button>
+      </form>
+      {tasksList && tasksList.map(todo => (
+        <ul key={todo.id}>
+          <li className={todo.complete ? 'task-li' : ''}>
+            {todo.task}
+          </li>
+            <button onClick={() => delteHandler(todo)}>Delete</button>
+            <button onClick={() => editHandler(todo.id, 'john')}>Edit</button>
+            <button onClick={() => completedHandler(todo.id)}>{todo.complete? 'Completed' : 'todo' }</button>
+        </ul>
+      ))}
     </div>
   );
 }
